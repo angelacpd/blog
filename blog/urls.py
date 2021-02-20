b1f5ad878a14bc0789a53cf16b1f574908cf794f
angelacpd <angelacrepaldi@gmail.com>
2021-02-20 11:50:28 -0300

    Add favicon.ico to app blog.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 
 from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
